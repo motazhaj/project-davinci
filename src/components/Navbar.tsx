f35a6b911f4ae2cecaefcb315b45f769d01d7029
Motaz HM <motazhajm@gmail.com>
2024-04-01 03:02:14 +0300

    Add navbar logo and update styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,21 @@
 const Navbar = () => {
+  const navItems = ["Products", "Projects", "About", "Contact"];
+
   return (
-    <nav className="w-full flex justify-between h-14 bg-lime-500 px-10 align-middle items-center">
-      <a href="" className="text-white text-2xl">
-        Green House
+    <nav className="w-full fixed z-50 flex justify-between h-14 shadow-xl bg-lime-500/60 backdrop-blur-md px-24 align-middle items-center">
+      <a href="" className="flex gap-4 text-white text-2xl items-center ">
+        <img className="h-9" src="./images/greenhouse-logo.png" alt="green house logo" />
+        <div className="font-bold">Green House</div>
       </a>
 
-      <ul className="flex gap-4 text-white">
-        <li>
-          <a href="">Products</a>
-        </li>
-        <li>
-          <a href="">Projects</a>
-        </li>
-        <li>
-          <a href="">About</a>
-        </li>
-        <li>
-          <a href="">Contact</a>
-        </li>
+      <ul className="flex gap-8 text-lg text-white font-bold items-center">
+        {navItems.map((item) => {
+          return (
+            <li className="hover:scale-125 hover:text-orange-300 transition-all">
+              <a href="">{item}</a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
