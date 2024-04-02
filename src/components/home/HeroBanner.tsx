d80b6bee156c34b09212372b14ad2b7f9ea1fac2
Motaz HM <motazhajm@gmail.com>
2024-04-02 22:59:10 +0300

    Update HeroBanner styling and add a button

--- src/components/home/HeroBanner.tsx
+++ src/components/home/HeroBanner.tsx
@@ -1,16 +1,30 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+
 const HeroBanner = () => {
   return (
-    <section className="bg-lime-950 h-[500px] relative">
-      <img
-        className="z-0 w-full h-full object-cover absolute blur-sm shadow-xl"
-        src="/images/paintshop.jpg"
-        alt="paintshop"
-      />
-      <div className="z-1 w-full h-full absolute text-white top-0 flex flex-col justify-center items-center text-center drop-shadow-2xl">
-        <h1 className="text-6xl font-bold">
-          Welcome to <span className="text-lime-400">Green House</span>
-        </h1>
-        <div className="text-2xl pt-4 font-bold">School and University Supplies, Projects, and Models</div>
+    <section className="">
+      <div className="bg-lime-950  h-[500px] w-full relative mt-20 rounded-[32px] overflow-hidden mx-auto">
+        <img
+          className="z-0 w-full h-full object-cover shadow-xl "
+          src="/images/paintshop.jpg"
+          alt="paintshop"
+        />
+        <div className="absolute w-full h-full top-0 left-0 flex justify-center items-center">
+          <div className="z-1 w-2/3 h-1/2 text-lime-800 flex flex-col justify-between text-center drop-shadow-2xl bg-white/70 rounded-[32px] px-10 pt-10">
+            <div>
+              <h1 className="text-5xl font-bold ">
+                Welcome to <span className="">Green House</span>
+              </h1>
+              <div className="text-2xl pt-4 font-bold">
+                School and University Supplies, Projects, and Models
+              </div>
+            </div>
+            <button className="flex items-center gap-2 mt-10 mb-6 mx-auto py-2 px-6 bg-orange-700 text-white mr-auto rounded-lg">
+              <FaLongArrowAltRight />
+              <div>See More!</div>
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
