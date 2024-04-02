de64d37ec13d03e223604321032f9d2f0d6a11c6
Motaz HM <motazhajm@gmail.com>
2024-04-02 22:58:48 +0300

    Update Navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,13 @@ const Navbar = () => {
   const navItems = ["Products", "Projects", "About", "Contact"];
 
   return (
-    <nav className="w-full fixed z-50 flex justify-between h-14 shadow-xl bg-lime-500/60 backdrop-blur-md px-24 align-middle items-center">
-      <a href="" className="flex gap-4 text-white text-2xl items-center ">
-        <img className="h-9" src="./images/greenhouse-logo.png" alt="green house logo" />
+    <nav className="w-full z-50 fixed flex justify-between h-14 backdrop-blur-md px-24 align-middle items-center text-green-800">
+      <a href="" className="flex gap-4 text-2xl items-center ">
+        <img className="h-9 drop-shadow-md" src="./images/greenhouse-logo.png" alt="green house logo" />
         <div className="font-bold">Green House</div>
       </a>
 
-      <ul className="flex gap-8 text-lg text-white font-bold items-center">
+      <ul className="flex gap-8 text-sm font-bold items-center">
         {navItems.map((item) => {
           return (
             <li className="hover:scale-125 hover:text-orange-300 transition-all">
