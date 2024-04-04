c8e3005fc1298fa9bf4777a3d80a3107713f8890
Motaz HM <motazhajm@gmail.com>
2024-04-04 14:50:22 +0300

    Update Featured collague

--- src/components/home/FeaturedCollague.tsx
+++ src/components/home/FeaturedCollague.tsx
@@ -2,12 +2,13 @@ import ImageTitled from "../shared/ImageTitled";
 
 const FeaturedCollague = () => {
   return (
-    <section className="p-4 flex flex-col gap-5">
+    <section className="p-4 max-w-[1400px] mx-auto flex flex-col gap-5">
       <div className="flex gap-5">
-        <ImageTitled />
-        <ImageTitled />
+        <ImageTitled title="Custom Projects" image="maquete.jpg" />
+        <ImageTitled title="Artworks" image="herobanner.jpg" />
+        <ImageTitled title="Printing Services" image="tshirt.jpg" />
       </div>
-      <ImageTitled />
+      <ImageTitled title="Store: School & Art supplies" image="schoolsup.jpg" />
     </section>
   );
 };
--- src/components/shared/ImageTitled.tsx
+++ src/components/shared/ImageTitled.tsx
@@ -1,16 +1,12 @@
-import React from "react";
-
-const ImageTitled = () => {
+const ImageTitled = ({ title, image }: { title: string; image: string }) => {
   return (
-    <div className="h-[400px] w-full overflow-hidden relative rounded-[16px] mx-auto">
-      <div>
-        <img
-          className="z-0 w-full h-full object-cover shadow-xl"
-          src="/images/herobanner.jpg"
-          alt="paintshop"
-        />
+    <div className="h-[400px] w-full overflow-hidden relative rounded-[16px] mx-auto drop-shadow-md cursor-pointer hover:scale-[1.05] transition-all">
+      <div className="w-full h-full">
+        <img className="z-0 w-full h-full object-cover shadow-xl" src={`/images/${image}`} alt="paintshop" />
       </div>
-      <h1 className="absolute top-6 left-6 text-4xl font-bold text-lime-900 drop-shadow-md">Some Title </h1>
+      <div className="w-full h-40 absolute bottom-0 bg-gradient-to-t from-white/70 to-transparent">
+        <h1 className="absolute bottom-6 left-6 text-4xl font-bold text-lime-900 drop-shadow-xl">{title}</h1>
+      </div>
     </div>
   );
 };
