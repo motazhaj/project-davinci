2634729b10fefe85fe903d102694f56c54fc9d76
Motaz HM <motazhajm@gmail.com>
2024-04-21 23:49:44 +0300

    Refactor categories props

--- src/components/home/Categories.tsx
+++ src/components/home/Categories.tsx
@@ -1,19 +1,13 @@
-import CategoryCard from "../product/CategoryCard";
+import CategoryCard, { categoryInterface } from "../product/CategoryCard";
 
-interface Category {
-  id: number;
-  category: string;
-  image: string;
-}
-
-const BestSellers = ({ list }: { list: Category[] }) => {
+const BestSellers = ({ list }: { list: categoryInterface[] }) => {
   return (
     <div className="w-full bg-gray-100 py-6 shadow-md">
       <div className="relative w-screen max-w-screen-lg mx-auto">
         <h1 className="text-3xl font-bold px-4">Categories</h1>
         <ul className="flex gap-4 justify-between my-4 overflow-x-scroll mx-4">
           {list.map((item) => {
-            return <CategoryCard key={item.id} category={item.category} image={item.image} />;
+            return <CategoryCard key={item.id} category={item} />;
           })}
         </ul>
       </div>
--- src/components/product/CategoryCard.tsx
+++ src/components/product/CategoryCard.tsx
@@ -1,16 +1,24 @@
-interface productCardProps {
-  category: string;
-  image: string;
+import { Link } from "react-router-dom";
+
+export interface categoryInterface {
+  id: number;
+  title: string;
+  image?: string;
 }
 
-const ProductCard = ({ category, image }: productCardProps) => {
+const ProductCard = ({ category }: { category: categoryInterface }) => {
+  const { title, image } = category;
   return (
     <li className="min-w-[230px] h-[240px] overflow-hidden relative rounded-xl ">
-      <a className="w-full h-full flex flex-col gap-2" href="">
-        <img className="h-full w-full object-cover" src={`/images/${image}`} alt="test" />
-        <h2 className="w-full px-4 pb-2 truncate absolute bottom-0 text-xl font-bold z-10">{category}</h2>
+      <Link to={`/category/${title}`} className="w-full h-full flex flex-col gap-2">
+        <img
+          className="h-full w-full object-cover"
+          src={image ? image : "/images/img-placeholder.png"}
+          alt="test"
+        />
+        <h2 className="w-full px-4 pb-2 truncate absolute bottom-0 text-xl font-bold z-10">{title}</h2>
         <div className="absolute bottom-0 w-full h-1/3 bg-gradient-to-t from-white/50" />
-      </a>
+      </Link>
     </li>
   );
 };
