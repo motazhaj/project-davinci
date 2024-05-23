f9b1a107dfebcc36af443391f011be60b7da9455
Motaz HM <motazhajm@gmail.com>
2024-05-23 15:27:44 +0300

    Refactor home and new arrivals

--- src/components/home/NewArrivals.tsx
+++ src/components/home/NewArrivals.tsx
@@ -1,15 +1,34 @@
-import ProductCard from "../product/ProductCard";
 import { productCardProps } from "../../utility/productsUtils";
+import ProductCard from "../product/ProductCard";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "../shared/LoadingSpinner";
 
-const NewArrivals = ({ list }: { list: productCardProps[] }) => {
+const NewArrivals = () => {
+  const [loading, setLoading] = useState(true);
+  const [newArrivals, setNewArrivals] = useState<productCardProps[] | []>([]);
+
+  useEffect(() => {
+    setLoading(false);
+    fetch("http://localhost:5000/new-arrivals")
+      .then((data) => data.json().then((data) => setNewArrivals(data)))
+      .catch((err) =>
+        console.warn("Something went wrong when fetching new arrivals: ", err)
+      )
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <div className="relative w-screen max-w-screen-lg mx-auto px-4">
       <h1 className="text-3xl font-bold">New Arrivals</h1>
-      <ul className="flex gap-4 justify-between mt-4 overflow-x-scroll">
-        {list.map((item) => {
-          return <ProductCard key={item.id} product={item} />;
-        })}
-      </ul>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <ul className="flex gap-4 justify-between mt-4 overflow-x-scroll">
+          {newArrivals.map((item) => {
+            return <ProductCard key={item.id} product={item} />;
+          })}
+        </ul>
+      )}
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,6 @@ import FeaturedCollague from "../components/home/FeaturedCollague";
 import HeroBanner from "../components/home/HeroBanner";
 import NewArrivals from "../components/home/NewArrivals";
 
-const newArrivals = [
-  { id: 0, title: "Outdoor Bag XL", category: "School Bag", price: "39.99" },
-  { id: 1, title: "Outdoor Bag XL", category: "School Bag", price: "39.99" },
-  { id: 2, title: "Outdoor Bag XL", category: "School Bag", price: "39.99" },
-  { id: 3, title: "Outdoor Bag XL", category: "School Bag", price: "39.99" },
-  { id: 4, title: "Outdoor Bag XL", category: "School Bag", price: "39.99" },
-];
 const categories = [
   { id: 0, title: "Note Books" },
   { id: 1, title: "Note Books" },
@@ -22,7 +15,7 @@ const Home = () => {
   return (
     <>
       <HeroBanner />
-      <NewArrivals list={newArrivals} />
+      <NewArrivals />
       <Categories list={categories} />
       <FeaturedCollague />
     </>
