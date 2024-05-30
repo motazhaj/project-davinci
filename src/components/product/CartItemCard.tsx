aed60929651458e6f43c57abf718c0552165643a
Motaz HM <motazhajm@gmail.com>
2024-05-31 00:24:37 +0300

    Refactor cart item display

--- src/components/product/CartItemCard.tsx
+++ src/components/product/CartItemCard.tsx
@@ -1,19 +1,51 @@
 import { Link } from "react-router-dom";
-import { cartItemInterface } from "../../utility/cartUtils";
+import { cartItemInterface, getLocalCart, removeCartItem, setItemQuantity } from "../../utility/cartUtils";
 import { formatNumber } from "../../utility/productsUtils";
 
-const CartItemCard = ({ cartItem }: { cartItem: cartItemInterface }) => {
+const CartItemCard = ({
+  cartItem,
+  setCartItems,
+}: {
+  cartItem: cartItemInterface;
+  setCartItems: React.Dispatch<React.SetStateAction<cartItemInterface[]>>;
+}) => {
   return (
-    <Link to={"/product/" + cartItem.id} className="flex gap-4">
-      <img src={cartItem.image} alt={cartItem.title} className="size-32 object-cover drop-shadow-md" />
-      <div className="flex flex-col">
-        <h2 className="truncate font-bold">{cartItem.title}</h2>
-        <p className="text-gray-400">{cartItem.category}</p>
-        <p>${formatNumber(cartItem.price)}</p>
-        <p>Qnt {cartItem.quantity}</p>
-        <p>Total Price ${formatNumber(cartItem.totalPrice)}</p>
+    <div className="flex gap-4 w-full">
+      <div className="flex w-full ">
+        <Link to={"/product/" + cartItem.id} className="w-5/12 flex gap-4 hover:scale-105 transition-transform">
+          <img src={cartItem.image} alt={cartItem.title} className="size-32 object-cover border rounded-lg" />
+          <div className="flex flex-col justify-between">
+            <h2 className="font-bold">{cartItem.title}</h2>
+            <p className="text-gray-400">{cartItem.category}</p>
+          </div>
+        </Link>
+        <p className="w-2/12">₪{formatNumber(cartItem.price)}</p>
+        <div className="w-2/12 flex items-start">
+          <input
+            className="bg-slate-200 rounded-lg p-2 text-center w-24 focus:outline outline-primary"
+            type="number"
+            value={cartItem.quantity}
+            onChange={(e) => {
+              setItemQuantity(cartItem.id, Number(e.target.value));
+              setCartItems(getLocalCart());
+            }}
+            step={1}
+          />
+        </div>
+        <p className="w-2/12">₪{formatNumber(cartItem.totalPrice)}</p>
+        <div className="w-1/12 flex gap-4 items-start">
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg"
+            onClick={() => {
+              removeCartItem(cartItem.id);
+              setCartItems(getLocalCart());
+            }}
+          >
+            Remove
+          </button>
+        </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
