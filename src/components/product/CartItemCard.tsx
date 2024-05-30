847daad4ecf24ae6e2d2f9a2e6b553fb32e2d527
Motaz HM <motazhajm@gmail.com>
2024-05-30 16:53:43 +0300

    Add cart item card component

---
+++ src/components/product/CartItemCard.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { cartItemInterface } from "../../utility/cartUtils";
+
+const CartItemCard = ({ cartItem }: { cartItem: cartItemInterface }) => {
+  return (
+    <Link to={"/product/" + cartItem.id} className="flex gap-4">
+      <img src={cartItem.image} alt={cartItem.title} className="size-32 object-cover drop-shadow-md" />
+      <div className="flex flex-col">
+        <h2 className="truncate font-bold">{cartItem.title}</h2>
+        <p className="text-gray-400">{cartItem.category}</p>
+        <p>${cartItem.price}</p>
+        <p>Qnt {cartItem.quantity}</p>
+        <p>Total Price ${cartItem.totalPrice}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default CartItemCard;
